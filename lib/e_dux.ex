9b283e8056c21bf14905ff1526b0e858a32a7a05
Steven Nunez <steven.nunez@gmail.com>
2017-04-12 16:43:59 -0400

    First pass

--- lib/e_dux.ex
+++ lib/e_dux.ex
@@ -1,18 +1,70 @@
 defmodule EDux do
-  @moduledoc """
-  Documentation for EDux.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
 
-  ## Examples
+  def start_link(reducerMap, initialState \\ nil) 
+  def start_link(reducerMap, initialState) when is_map(reducerMap) do
+    GenServer.start_link(__MODULE__, [reducerMap, initialState])
+  end
 
-      iex> EDux.hello
-      :world
+  def start_link(reducerModule, initialState) do
+    GenServer.start_link(__MODULE__, [reducerModule, initialState])
+  end
 
-  """
-  def hello do
-    :world
+  def dispatch(store, action) do
+    GenServer.cast(store, {:dispatch, action})
+  end
+
+  def get_state(store) do
+    GenServer.call(store, {:get_state})
+  end
+
+  def subscribe(store, listener) do
+    GenServer.cast(store, {:subscribe, listener})
+  end
+
+  def init([reducerMap = %{}, nil]), do: init([reducerMap, %{}])
+  def init([reducerMap = %{}, initialState]) do
+    reducers = Enum.reduce(reducerMap, %{}, fn ({label, reducerModule}, map) ->
+    {:ok, reducer} = EDux.start_link(reducerModule, Map.get(initialState, label))
+    Map.put(map, label, reducer)
+    end)
+    {:ok, %{combined_reducers: reducers}}
+  end
+
+  def init([reducerModule, nil]) do
+    state = apply(reducerModule, :reduce, [nil, %{type: "@@EDux/INIT"}])
+    {:ok, %{reducer: reducerModule, listeners: [], state: state }}
+  end
+
+  def init([reducerModule, initialState]) do
+    {:ok, %{reducer: reducerModule, listeners: [], state: initialState }}
+  end
+
+  def handle_call({:get_state}, _from, %{state: state} = state_data) do
+    {:reply, state, state_data}
+  end
+
+  def handle_call({:get_state}, _from, %{combined_reducers: reducers} = state_data) do
+    state = Enum.reduce(reducers, %{}, fn ({label, reducer}, map) -> 
+      Map.put(map, label, EDux.get_state(reducer))
+    end)
+    {:reply, state, state_data}
+  end
+
+  def handle_cast({:dispatch, action}, %{combined_reducers: reducers} = state_data) do
+    for r <- Map.values(reducers), do: EDux.dispatch(r, action)
+    {:noreply, state_data}
+  end
+
+  def handle_cast({:dispatch, action}, %{state: state, reducer: reducer, listeners: listeners} = state_data) do
+    state = apply(reducer, :reduce, [state, action])
+    for l <- listeners, do: l.()
+    {:noreply, %{state_data | state: state}}
+  end
+
+  def handle_cast({:subscribe, listener}, %{listeners: listeners} = state_data) do
+    {:noreply, %{state_data | listeners: [listener | listeners]}}
   end
 end
+
--- test/e_dux_test.exs
+++ test/e_dux_test.exs
@@ -1,8 +1,94 @@
 defmodule EDuxTest do
   use ExUnit.Case
-  doctest EDux
+  alias Test.CounterReducer
+  alias Test.FriendsReducer
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "Single Reducer" do
+    test "Getting State" do
+      {:ok, counter} = EDux.start_link(CounterReducer)
+      assert EDux.get_state(counter) == 0
+    end
+    
+    test "Does nothing for bogus messages" do
+      {:ok, store} = EDux.start_link(CounterReducer)
+      EDux.dispatch(store, %{type: "TOTALLY BOGUS"})
+      assert EDux.get_state(store) == 0
+    end
+
+    test "Increments state" do
+      {:ok, store} = EDux.start_link(CounterReducer)
+      EDux.dispatch(store, %{type: "INCREMENT"})
+      assert EDux.get_state(store) == 1
+    end
+
+    test "Decrements state" do
+      {:ok, store} = EDux.start_link(CounterReducer)
+      EDux.dispatch(store, %{type: "DECREMENT"})
+      assert EDux.get_state(store) == -1
+    end
+
+    test "notifies subscribers of changes" do
+      Process.register self(), :test
+      {:ok, store} = EDux.start_link(CounterReducer)
+      EDux.subscribe(store, fn () -> 
+        send :test, :called_callback
+      end)
+      EDux.dispatch(store, %{type: "DECREMENT"})
+      assert_receive :called_callback
+    end
   end
+  describe "Multiple Reducers" do
+    test "Getting State" do
+      {:ok, store} = EDux.start_link(%{counter: CounterReducer, friends: FriendsReducer})
+      %{counter: counterState, friends: friendsState} = EDux.get_state(store)
+      assert counterState == 0
+      assert friendsState == []
+    end
+
+    test "Does nothing for bogus messages" do
+      {:ok, store} = EDux.start_link(%{counter: CounterReducer, friends: FriendsReducer})
+      EDux.dispatch(store, %{type: "TOTALLY BOGUS"})
+      %{counter: counterState, friends: friendsState} = EDux.get_state(store)
+      assert counterState == 0
+      assert friendsState == []
+    end
+
+    test "Changes state" do
+      {:ok, store} = EDux.start_link(%{counter: CounterReducer, friends: FriendsReducer})
+      EDux.dispatch(store, %{type: "INCREMENT"})
+      %{counter: counterState, friends: friendsState} = EDux.get_state(store)
+      assert counterState == 1
+      assert friendsState == []
+
+      EDux.dispatch(store, %{type: "ADD_FRIEND", friend: %{name: "Steven", id: "1"}})
+      %{counter: counterState, friends: friendsState} = EDux.get_state(store)
+      assert counterState == 1
+      assert friendsState == [%{id: "1", name: "Steven"}]
+    end
+
+    test "Takes initial state properly" do
+      {:ok, store} = EDux.start_link(
+                      %{counter: CounterReducer, friends: FriendsReducer},
+                      %{counter: 42, friends: [%{id: "42", name: "Steven"}]})
+      %{counter: counterState, friends: friendsState} = EDux.get_state(store)
+      assert counterState == 42
+      assert friendsState == [%{id: "42", name: "Steven"}]
+    end
+  end
+end
+
+defmodule Test.CounterReducer do
+  def reduce(_, %{type: "@@EDux/INIT"}),    do: 0
+  def reduce(state, %{type: "INCREMENT"}),  do: state + 1
+  def reduce(state, %{type: "DECREMENT"}),  do: state - 1
+  def reduce(state, _),                     do: state 
+end
+
+defmodule Test.FriendsReducer do
+  def reduce(_,     %{type: "@@EDux/INIT"}), do: []
+  def reduce(state, %{type: "ADD_FRIEND", friend: friend}), do: state ++ [friend]
+  def reduce(state, %{type: "REMOVE_FRIEND", id: id}) do 
+    for i <- state, i.id != id, do: i
+  end
+  def reduce(state, _), do: state
 end
